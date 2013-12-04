a505db152d31d8783abda21b8198ca6639dea282
OpenSauce <lawleycobhc@gmail.com>
2013-12-04 16:48:49 +0100

    Restructured directory

---
+++ src/APIConnector.java
@@ -0,0 +1,9 @@
+
+public class APIConnector {
+	final private String API_KEY;
+
+	public APIConnector(String API_KEY) {
+		this.API_KEY = API_KEY;
+	}
+	
+}
--- src/DotA2API.java
+++ src/DotA2API.java
@@ -2,8 +2,7 @@
 public class DotA2API {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new APIConnector("");
 	}
 
 }
