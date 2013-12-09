5d39934a09732a8ea3a61a6de2c50a2bdfa73be5
OpenSauce <lawleycobhc@gmail.com>
2013-12-09 10:38:39 +0100

    APIHandler.retrieveFullMatchHistory implemented.

--- src/APIHandler.java
+++ src/APIHandler.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.io.StringReader;
-import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -8,65 +6,57 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
-
 import org.apache.commons.io.IOUtils;
 
 
 public class APIHandler {
-	private Format defaultFormat;
 	private int fetchInterval = 500;
 	private int matchesRequested = 25;
 	final private String API_KEY;
-	final private String URL_BASE = 
+	final private String URL_MATCH_HISTORY_BASE = 
 			"https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/";
 
 	public APIHandler(String API_KEY) {
 		this.API_KEY = API_KEY;
-		this.defaultFormat = Format.JSON;
 	}
 	
-	public APIHandler(String API_KEY, Format defaultFormat) {
-		this.API_KEY = API_KEY;
-		this.defaultFormat = defaultFormat;
-	}
-	
-	private JsonElement retrieveJsonElement(String siteParameters) {
+	private ArrayList<Match> retrieveMatchesFromJson(String siteParameters) {
+		ArrayList<Match> matchList = new ArrayList<Match>();
 		JsonElement jsonElement = null;
 		try {
-			String jsonString = IOUtils.toString(new URL(URL_BASE + 
+			String jsonString = IOUtils.toString(new URL(URL_MATCH_HISTORY_BASE + 
 					"?key=" + API_KEY + siteParameters));
 			JsonParser jsonParser = new JsonParser();
 			jsonElement = jsonParser.parse(jsonString);
-		} catch(IOException e) {
-			
-		}
-		return jsonElement;
-	}
-	
-	public ArrayList<Match> retrieveFullMatchHistory(DotaEntity entity) {
-		ArrayList<Match> retrievedMatchList = new ArrayList<Match>();
-		if(entity instanceof Player) {
-			Player thisPlayer = (Player) entity;
-			JsonElement jsonElement = retrieveJsonElement("&account_id=" + thisPlayer.getAccountID());
 			if(jsonElement.isJsonObject()) {
 				JsonObject result = jsonElement.getAsJsonObject().getAsJsonObject("result");
-				JsonArray matchList = result.getAsJsonArray("matches");
-				for(JsonElement matchListElement : matchList) {
+				JsonArray jsonMatchList = result.getAsJsonArray("matches");
+				for(JsonElement matchListElement : jsonMatchList) {
 					Match match = new Match(matchListElement.getAsJsonObject().get("match_id").getAsString());
 					match.setLobbyType(matchListElement.getAsJsonObject().get("lobby_type").getAsString());
 					match.setMatchSeqNum(matchListElement.getAsJsonObject().get("match_seq_num").getAsString());
 					match.setStartTime(matchListElement.getAsJsonObject().get("start_time").getAsLong());
-					retrievedMatchList.add(match);
+					matchList.add(match);
 				}
+				return matchList;
 			}
+		} catch(IOException e) {
+			System.out.println("Failed to connect to WebAPI!");
+		}
+		return null;
+	}
+	
+	public ArrayList<Match> retrieveFullMatchHistory(DotaEntity entity) {
+		if(entity instanceof Player) {
+			Player thisPlayer = (Player) entity;
+			return retrieveMatchesFromJson("&account_id=" + thisPlayer.getAccountID());	
 		} else if (entity instanceof Hero) {
-			
+			Hero thisHero = (Hero) entity;
+			return retrieveMatchesFromJson("&hero_id=" + thisHero.getID());	
 		} else {
 			System.out.println("Unknown instance of DotaEntity.");
+			return null;
 		}
-		
-		return retrievedMatchList;
 	}
 	
 	public ArrayList<Match> retrieveMatchHistory(DotaEntity entity, SearchConstraints constraints) {
--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,17 @@
 
 public class Hero extends DotaEntity {
+	private String ID;
+	
+	public Hero(String ID) {
+		this.ID = ID;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
 
 }
